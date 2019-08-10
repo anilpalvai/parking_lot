3969a3ff53999ac4f9918d960c3e5a511e8b1c2e
anilpalwai <anil.palvai@gmail.com>
2019-08-10 19:21:10 +0800

    adding Empty Maven project to codebase

---
+++ src/main/java/com/dev/r1/App.java
@@ -0,0 +1,13 @@
+package com.dev.r1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
